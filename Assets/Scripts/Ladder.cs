9dca73014daac3cc34aa4b95927ccca6d78aefec
EnSung <dmstjd0911@naver.com>
2023-08-04 19:57:41 +0900

    :sparkles: :: 사다리 outline 추가

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -13,5 +13,5 @@ public class Ladder : MonoBehaviour
     [SerializeField] Transform _ladderSpawnPos;
     public Transform LadderSpawnPos { get { return _ladderSpawnPos; } }
 
-
+    public GameObject Outline;
 }

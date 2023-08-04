a8b21d4469811a74a8438869f545f5aa0f3b79e4
EnSung <dmstjd0911@naver.com>
2023-08-04 17:38:30 +0900

    :art: :: 사다리 설치/회수 쿨타임 추가

--- Assets/Scripts/Player/PlayerLadderController.cs
+++ Assets/Scripts/Player/PlayerLadderController.cs
@@ -11,15 +11,25 @@ public class PlayerLadderController : NetworkBehaviour
     [SerializeField] private LayerMask _ladderLayerMask;
 
     [SerializeField] Stack<Ladder> _myLadders = new Stack<Ladder>();
-    void Start()
-    {
-        
-    }
 
+
+    [Header("Cooltime")]
+    [SerializeField] private float _installCooltime;
+    [SerializeField] private float _recallCooltime;
+    TickTimer _ladderInstallTimer { get; set; }
+    TickTimer _ladderRecallTimer { get; set; }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position,_sensingRadius);
+    }
+
+    public override void Spawned()
+    {
+        base.Spawned();
+        _ladderInstallTimer = TickTimer.CreateFromSeconds(Runner, 0);
+        _ladderRecallTimer = TickTimer.CreateFromSeconds(Runner, 0);
     }
     public override void FixedUpdateNetwork()
     {
@@ -29,6 +39,7 @@ public class PlayerLadderController : NetworkBehaviour
             if (input.GetButton(InputButton.INSTALL))
             {
                 if (_ladderAmount <= 0) return;
+                if (_ladderInstallTimer.Expired(Runner) == false) return;
 
                 Collider2D col = SenseLadder(_sensingRadius, _ladderLayerMask);
 
@@ -37,16 +48,20 @@ public class PlayerLadderController : NetworkBehaviour
                 else ladder = LadderManager.Instance.InstallContinuedLadder(col.gameObject.GetComponentInChildren<Ladder>());
 
                 _myLadders.Push(ladder);
+
+                _ladderInstallTimer = TickTimer.CreateFromSeconds(Runner, _installCooltime);
             }
             if (input.GetButton(InputButton.RECALL))
             {
                 if (_myLadders.Count <= 0) return;
+                if (_ladderRecallTimer.Expired(Runner) == true) return;
 
                 if (_myLadders.Peek().NextLadder != null) return;
 
                 Ladder myLadder = _myLadders.Pop();
 
                 LadderManager.Instance.RecallLadder(myLadder);
+                _ladderRecallTimer = TickTimer.CreateFromSeconds(Runner, _installCooltime);
             }
         }
         
